63f8935c0c5c7b79c8fec855ec7ab6d1e36efe41
Roger Nordquist <roger.nordquist@gmail.com>
2016-10-23 16:10:07 +0200

    adding sql script

---
+++ mariadb/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `modernlibs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
