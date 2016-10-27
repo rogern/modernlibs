d94587a55ba22ba4c8bc99576ef39494937ef52a
Roger Nordquist <roger.nordquist@gmail.com>
2016-10-27 20:48:26 +0200

    made it a bit more readable

--- src/main/scala/org/modernlibs/ModernLibsService.scala
+++ src/main/scala/org/modernlibs/ModernLibsService.scala
@@ -11,17 +11,31 @@ class ModernLibsService(db: DB) {
   val modernLibsService = HttpService {
     case GET -> Root / "ml" / name => getPerson(name)
     case GET -> Root / "ml" => getPersons
-    case r @ POST -> Root / "ml" => newPerson(r)
+    case r@POST -> Root / "ml" => newPerson(r)
   }
 
-  private def getPersons = db.list().flatMap(_.map(r => Ok(r.asJson)) | InternalServerError())
+  private def getPersons = {
+    for {
+      dbResult <- db.list()
+      result <- dbResult.map(r => Ok(r.asJson)) | InternalServerError()
+    } yield result
+  }
 
-  private def getPerson(name: String) = db.find(name).flatMap(_.map(_.fold(NotFound())(r => Ok(r.asJson))) | InternalServerError())
+  private def getPerson(name: String) = {
+    for {
+      dbResult <- db.find(name)
+      result <- dbResult.map(_.fold(NotFound())(r => Ok(r.asJson))) | InternalServerError()
+    } yield result
+  }
 
   private def newPerson(r: Request) = {
-    r.as(jsonOf[Person]).flatMap(p => db.save(p).flatMap(_.map {
-      case 1 => Created()
-      case _ => Conflict()
-    } | InternalServerError()))
+    for {
+      person <- r.as(jsonOf[Person])
+      dbResponse <- db.save(person)
+      response <- dbResponse.map {
+        case 1 => Created()
+        case _ => Conflict()
+      } | InternalServerError()
+    } yield response
   }
 }
